47ec9c8b3bcf0f328af193719157e2e33d17edbe
sana-ketar <sanamouamar@gmail.com>
2025-09-27 10:00:07 +0200

    modification

--- src/main/java/com/retrotoon/retrotoon/contoller/MainController.java
+++ src/main/java/com/retrotoon/retrotoon/contoller/MainController.java
@@ -1,12 +1,15 @@
 package com.retrotoon.retrotoon.contoller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.retrotoon.retrotoon.dtos.UserRequestDto;
+import com.retrotoon.retrotoon.model.Utilisateur;
 import com.retrotoon.retrotoon.service.UtilisateurService;
 
 
@@ -16,9 +19,11 @@ public class MainController {
     private UtilisateurService utilisateurService;
 
     @GetMapping("/inscription")
-    public String inscription(){
+    public String inscription(Model model) {
+        model.addAttribute("user", new UserRequestDto());
         return "html/inscription";
     }
+
     @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("user", new UserRequestDto());
@@ -39,5 +44,40 @@ public class MainController {
     public String connexion(){
         return "html/connexion";
     }
-
+    @GetMapping("/contact")
+    public String contact(){
+        return "html/contact";
+    }
+    @GetMapping("/films")
+    public String films(){
+        return "html/films";
+    }
+    @GetMapping("/series")
+    public String series(){
+        return "html/series";
+    }
+    @GetMapping("/parametre")
+    public String parametre(){
+        return "html/parametre";
+    }
+    @GetMapping("/profil")
+    public String profil(){
+        return "html/profil";
+    }
+    @GetMapping("/dashbord")
+    public String dashbord(){
+        return "html/dashbordAdmin";
+    }
+    @GetMapping("/upload")
+    public String upload(){
+        return "html/uploadvideo";
+    }
+    @GetMapping("/index")
+    public String index(){
+        return "html/index";
+    }
+    @GetMapping("/video")
+    public String video(){
+        return "html/fiche-dessin-anime";
+    }
 }
