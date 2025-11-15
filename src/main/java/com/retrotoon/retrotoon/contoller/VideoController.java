f6a71077053f79132304e5d8dd11774d539bb318
sana <sanamouamar@gmail.com>
2025-11-15 15:50:35 +0100

    fix: last modifications

--- src/main/java/com/retrotoon/retrotoon/contoller/VideoController.java
+++ src/main/java/com/retrotoon/retrotoon/contoller/VideoController.java
@@ -2,6 +2,7 @@ package com.retrotoon.retrotoon.contoller;
 
 import java.util.List;
 
+import com.retrotoon.retrotoon.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,24 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.retrotoon.retrotoon.dtos.VideoCreateDto;
 import com.retrotoon.retrotoon.model.Video;
-import com.retrotoon.retrotoon.service.VideoServiceImpl;
 
 @RestController
 @RequestMapping("/videos")
 public class VideoController {
     
     @Autowired
-    VideoServiceImpl videoServiceImpl;
+    VideoService videoService;
 
     @PostMapping("/uploadVideo")
     public ResponseEntity<Video> createVideo(@RequestBody VideoCreateDto videoCreateDto){
-        Video savedVideo = videoServiceImpl.addNewVideo(videoCreateDto);
+        Video savedVideo = videoService.addNewVideo(videoCreateDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedVideo);
     }
     
     @GetMapping("/all")
     public ResponseEntity<List<VideoCreateDto>> getAll(){
-        List<VideoCreateDto> videos = videoServiceImpl.getAllVideos();
+        List<VideoCreateDto> videos = videoService.getAllVideos();
         if (videos.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
@@ -42,7 +42,7 @@ public class VideoController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Video> getById(@PathVariable Long id){
-        Video video = videoServiceImpl.getVideoById(id);
+        Video video = videoService.getVideoById(id);
         if (video == null) {
             return ResponseEntity.notFound().build();
         }
@@ -51,8 +51,8 @@ public class VideoController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Video> updateVideo(@PathVariable Long id, @RequestBody VideoCreateDto updatedVideo){
-        Video video = videoServiceImpl.getVideoById(id);
-        Video updateVideo = videoServiceImpl.updateVideo(id, updatedVideo);
+        Video video = videoService.getVideoById(id);
+        Video updateVideo = videoService.updateVideo(id, updatedVideo);
         if (video == null) {
             return ResponseEntity.notFound().build();
         }
@@ -62,7 +62,7 @@ public class VideoController {
     @DeleteMapping("/delete")
     public ResponseEntity<Void> deleteVideo(@RequestBody List<Long> ids){
         try{
-        ids.forEach(id -> videoServiceImpl.deleteVideoById(id));
+        ids.forEach(id -> videoService.deleteVideoById(id));
             return ResponseEntity.noContent().build();
         }catch(Exception e){
         return ResponseEntity.notFound().build();
